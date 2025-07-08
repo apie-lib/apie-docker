488033f958db62602f828aa508346111510d4e5c
Pieter Jordaan <pieter_jordaan@hotmail.com>
2025-07-08 10:58:55 +0200

    add missing files, fix dockerfile

---
+++ symfony/public/index.php
@@ -0,0 +1,8 @@
+// public/index.php
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context): Kernel {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};

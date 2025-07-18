65cc57f597affb1a269b2b86a06dd5b9bb63a2e9
Pieter Jordaan <pieter_jordaan@hotmail.com>
2025-07-18 09:08:50 +0200

    Generate a proper Laravel key on starting the container

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@ set -e
 
 echo "Starting container with FRAMEWORK=${FRAMEWORK} and PHP_VARIANT=${PHP_VARIANT}"
 
+if [ "$FRAMEWORK" = "laravel" ]; then
+  echo "Generate app key"
+  exec php artisan key:generate
+fi
+
 if [ "$PHP_VARIANT" = "cli" ]; then
   echo "Running in CLI mode (built-in PHP server)"
   exec php -S 0.0.0.0:8000 -t public

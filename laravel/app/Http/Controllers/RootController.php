4e6fdf8af99775fe7b5c7fb65e46a6991fa8d265
Pieter Jordaan <pieter_jordaan@hotmail.com>
2025-07-22 16:11:36 +0200

    Get rid of correct redirect in RootController

--- laravel/app/Http/Controllers/RootController.php
+++ laravel/app/Http/Controllers/RootController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Apie\Core\BoundedContext\BoundedContextHashmap;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
 
 class RootController
 {
-    public function __invoke(): RedirectResponse
+    public function __invoke(BoundedContextHashmap $hashmap): RedirectResponse
     {
-        try {
-            return new RedirectResponse(route('apie.example.cms.dashboard'));
-        } catch (RouteNotFoundException) {
-            return new RedirectResponse(route('apie.example.swagger_ui'));
+        foreach ($hashmap as $key => $value) {
+            return new RedirectResponse(route('apie.' . $key . '.cms.dashboard'));
         }
+        return new Response("No entities found. Pls run this in a terminal:<br /><code>docker exec myapp php artisan apie:create-domain-object -b example Test</code>");
     }
 }
--- symfony/src/Controller/RootController.php
+++ symfony/src/Controller/RootController.php
@@ -2,20 +2,20 @@
 
 namespace App\Controller;
 
+use Apie\Core\BoundedContext\BoundedContextHashmap;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/')]
 class RootController
 {
-    public function __invoke(UrlGeneratorInterface $urlGenerator): RedirectResponse
+    public function __invoke(UrlGeneratorInterface $urlGenerator, BoundedContextHashmap $hashmap): RedirectResponse
     {
-        try {
-            return new RedirectResponse($urlGenerator->generate('apie.example.cms.dashboard'));
-        } catch (RouteNotFoundException) {
-            return new RedirectResponse($urlGenerator->generate('apie.example.swagger_ui'));
+        foreach ($hashmap as $key => $value) {
+            return new RedirectResponse($urlGenerator->generate('apie.' . $key . '.cms.dashboard'));
         }
+        return new Response("No entities found. Pls run this in a terminal:<br /><code>docker exec myapp php bin/console apie:create-domain-object -b example Test</code>");
     }
 }
